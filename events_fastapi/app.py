f8dcc85d8c78b4df8caf68126064b63f852b2ed8
KL <100872126+KayoLeanndro@users.noreply.github.com>
2024-01-15 19:59:36 +0000

    testing get and post

---
+++ events_fastapi/app.py
@@ -0,0 +1,30 @@
+from typing import List 
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+database = []
+
+class  Events(BaseModel):
+    nome: str
+    dono: str
+    descricao: str
+    data: str
+    quantidade_ingressos: int
+    foi_vendido: bool
+    data_validade: str
+
+@app.get('/')
+async def hello_world() -> str:
+    return 'hello world'
+
+@app.post('/event', response_model=Events)
+async def criar_evento(event: Events):
+    database.append(event)
+    return event
+
+@app.get('/event')
+async def ler_todos_os_eventos() -> List[Events]:
+    return database
